4dacfb4df43187b9bcf53c8e67dfa1d2d48696ae
fpestunov <fpestunov@gmail.com>
2019-07-21 10:36:53 +0400

    add solution for 1

--- spec/Acme/FizzBuzzSpec.php
+++ spec/Acme/FizzBuzzSpec.php
@@ -8,8 +8,8 @@ use Prophecy\Argument;
 
 class FizzBuzzSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_translates_1_for_fizzbuzz()
     {
-        $this->shouldHaveType(FizzBuzz::class);
+        $this->execute(1)->shouldReturn(1);
     }
 }
--- src/Acme/FizzBuzz.php
+++ src/Acme/FizzBuzz.php
@@ -4,4 +4,8 @@ namespace Acme;
 
 class FizzBuzz
 {
+    public function execute()
+    {
+        return 1;
+    }
 }
